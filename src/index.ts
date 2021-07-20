32c8093ed2a5cf4a7a938c81b20f72804a7ac372
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-19 21:38:12 -0700

    implement PUT /v1/:org/repositories/to_file

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { CacheType } from './interfaces';
 import { GitHubService, GitHubServiceOps } from './services';
 
 import * as fastify from 'fastify';
+import { writeCompressedFile } from './utils/helpers';
 
 const gitHubServiceOpts: GitHubServiceOps = {
     cacheType: CacheType.FILE,
@@ -12,6 +13,7 @@ const ghService = new GitHubService(gitHubServiceOpts);
 
 const API_VERSION = '/v1';
 const SERVER_PORT = 8080;
+const TMP_FILE_PATH = '/tmp/knock_interview.json.gz';
 
 interface OrgParams {
     org: string;
@@ -43,6 +45,34 @@ server.get<{ Params: OrgParams }>(
             .code(200)
             .header('Content-Type', 'application/json; charset=utf-8')
             .send(orgRepos);
+    },
+);
+
+server.put<{ Params: OrgParams }>(
+    `${API_VERSION}/:org/repositories/to_file`,
+    {},
+    async (request, reply) => {
+        const org = request.params.org;
+
+        // TODO: validation middleware, to be DRY
+        if (org !== 'google') {
+            reply
+                .code(400)
+                .send(
+                    `Unable to fetch info for org "${org}". Only "google" is supported.\n`,
+                );
+            return;
+        }
+
+        let orgRepos;
+        try {
+            orgRepos = await ghService.getOrgRepos(org);
+            await writeCompressedFile(orgRepos, TMP_FILE_PATH);
+        } catch {
+            reply.code(500).send('Internal error => LOL\n');
+        }
+
+        reply.code(201).send();
     },
 );
 
