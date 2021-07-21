c619be55c6c65c317f3ec7b9b99603bcd3a6a048
SpaceKatt <spacekattpoispin@gmail.com>
2021-07-20 17:35:38 -0700

    add unit tests for FileCache

---
+++ src/cache/fileCache.unit.test.ts
@@ -0,0 +1,59 @@
+import { writeCompressedFile } from '../utils';
+import { FileCache } from './fileCache';
+
+const TESTING_CACHE_NAME = 'lmao_test_cache';
+const sampleKey = 'ree';
+const sampleData = {
+    foo: 'bar',
+};
+
+describe('Testing FileCache', () => {
+    const fCache = new FileCache(TESTING_CACHE_NAME);
+
+    beforeAll(() => {
+        fCache.flush();
+    });
+
+    afterAll(() => {
+        fCache.flush();
+    });
+
+    afterEach(() => {
+        fCache.flush();
+    });
+
+    it('Returns undefined on cache miss', async () => {
+        const c = await fCache.get('BAD_KEY');
+
+        expect(c).toBeUndefined();
+    });
+
+    it('Returns undefined on cache miss on nonempty cache', async () => {
+        await fCache.set('keyOne', sampleData);
+        await fCache.set('keyTwo', sampleData);
+        await fCache.set('keyThree', sampleData);
+        const c = await fCache.get('BAD_KEY');
+
+        expect(c).toBeUndefined();
+    });
+
+    it('Returns data stored in cache', async () => {
+        await fCache.set(sampleKey, sampleData);
+        const c = await fCache.get(sampleKey);
+
+        expect(c).toEqual(sampleData);
+    });
+
+    it('Data deleted on flush', async () => {
+        await fCache.set(sampleKey, sampleData);
+        const c = await fCache.get(sampleKey);
+
+        expect(c).toEqual(sampleData);
+
+        fCache.flush();
+
+        const c2 = await fCache.get(sampleKey);
+
+        expect(c2).toBeUndefined();
+    });
+});
